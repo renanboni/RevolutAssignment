a95af6aca4802b2dae9e1466bd9cdc24b97e6ae9
renanboni <renan.boni@usp.br>
2020-05-06 16:45:16 +0200

    fied error

--- app/src/main/java/com/example/revolutassingment/features/currencies/CurrencyFragment.kt
+++ app/src/main/java/com/example/revolutassingment/features/currencies/CurrencyFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.revolutassingment.R
 import com.example.revolutassingment.domain.entities.Rate
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.main.fragment_currency.currencies
-import kotlinx.android.synthetic.main.fragment_currency.loading
+import kotlinx.android.synthetic.main.fragment_currency.*
 import javax.inject.Inject
 
 class CurrencyFragment : DaggerFragment(), OnRateValueChanged {
@@ -47,9 +47,19 @@ class CurrencyFragment : DaggerFragment(), OnRateValueChanged {
         super.onViewCreated(view, savedInstanceState)
 
         setupAdapter()
+        setupViews()
 
         viewModel.ratesViewState.observe(viewLifecycleOwner, Observer { renderRates(it) })
         viewModel.errorViewState.observe(viewLifecycleOwner, Observer { renderError() })
+    }
+
+    private fun setupViews() {
+        reload.setOnClickListener {
+            viewModel.getRates(shouldUpdate = true)
+            loading.visibility = View.VISIBLE
+            reload.visibility = View.GONE
+            currencies.visibility = View.GONE
+        }
     }
 
     private fun setupAdapter() {
@@ -59,11 +69,15 @@ class CurrencyFragment : DaggerFragment(), OnRateValueChanged {
 
     private fun renderRates(rates: MutableList<Rate>) {
         loading.visibility = View.GONE
+        currencies.visibility = View.VISIBLE
         adapter.submitList(rates)
     }
 
     private fun renderError() {
         Toast.makeText(requireContext(), getString(R.string.error), Toast.LENGTH_LONG).show()
+        reload.visibility = View.VISIBLE
+        loading.visibility = View.GONE
+        currencies.visibility = View.GONE
     }
 
     override fun onRateChanged(currency: String, value: Double) {
